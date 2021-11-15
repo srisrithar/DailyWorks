9c2411fa6274e0e97650533d06a5243fedec6e67
srisrithar <sridan0001@gmail.com>
2021-11-15 14:24:27 +0530

    import with public method call activity on another class in Encapsulation Package level access

---
+++ Worker.java
@@ -0,0 +1,16 @@
+package tamilnadu.madurai1;
+
+import tamilnadu.tamilnadu.Office1;
+
+public class Worker
+{
+public static void main(String[] args)
+{
+Office1 w1 = new Office1();
+w1.work();
+	//w1.giveSnacks();
+	//System.out.println(w1.ta);
+	
+}
+
+}
