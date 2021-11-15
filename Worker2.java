9034dae5d1d9fdcab197f4902a838cccf435e208
srisrithar <sridan0001@gmail.com>
2021-11-15 14:30:52 +0530

    import without public method in same package name on Office1 class

---
+++ Worker2.java
@@ -0,0 +1,15 @@
+package tamilnadu.tamilnadu;
+
+public class Worker2
+{
+
+public static void main(String[] args)
+{
+Office1 w1 = new Office1();
+	w1.work();
+	w1.giveSnacks();
+	System.out.println(w1.ta);
+	
+}
+
+}
