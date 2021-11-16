a92200a7ff200b1796f67670dbe97884a89cb833
srisrithar <sridan0001@gmail.com>
2021-11-16 18:50:16 +0530

    Led Extends with Bulb for Nextgen using Extends keyword

---
+++ Led.java
@@ -0,0 +1,30 @@
+package nextgen;
+
+import nineties.Bulb;
+
+public class Led extends Bulb
+
+{
+public static void main(String[] args)
+{
+Led philips = new Led();
+philips.switchon();
+philips.switchoff();
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
