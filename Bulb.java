70490747edd88450775f7e0000aba4852b17281d
srisrithar <sridan0001@gmail.com>
2021-11-16 18:42:34 +0530

    Nineties generation To be Exends Nextgen

---
+++ Bulb.java
@@ -0,0 +1,19 @@
+package nineties;
+
+public class Bulb
+{
+public static void main(String[] args)
+{
+
+
+
+}
+public void switchon()
+{
+System.out.println("Lighton");
+}
+public void switchoff()
+{
+System.out.println("Lightoff");
+}
+}
