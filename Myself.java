459ef2b12eeeec72ab6346a875825781334754e0
srisrithar <sridan0001@gmail.com>
2021-11-20 18:59:18 +0530

    used in Package name as Nativeplace.Grandmother because Myself is out of relationship instead of extends keyword

---
+++ Myself.java
@@ -0,0 +1,12 @@
+//package nativeplace;
+package kovai;
+//import nativeplace.GrandMother;
+public class Myself
+{
+public static void main(String[] args)
+{
+GrandMother m = new GrandMother();
+m.invite();
+
+}
+}
